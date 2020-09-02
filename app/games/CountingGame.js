664df0f90f9481ae77bff5a678e03c9472b43a8c
Marco Chavez <marcode.chavez.jr@gmail.com>
2020-09-01 20:13:06 -0700

    adding a sound handler

--- app/games/CountingGame.js
+++ app/games/CountingGame.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+const numbers = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10
+]
+
 export default function CountingGame() {
     return (
         <View style={styles.container}>
